5b4186638de403760b3f85c0ac1b8b27e381c41a
Chris <aldershotchris@googlemail.com>
2017-11-10 14:17:48 +0000

    Added DFS Folder and first sample

---
+++ dfs/CreateDFSFolder.ps1
@@ -0,0 +1,4 @@
+﻿    #Create a new DFS Folder with two targets and assign ABE permissions.
+    New-DFSNFolder -Path "$NewDFSFolderPath" -TargetPath "$PrimaryTarget" -ReferralPriorityClass GlobalHigh
+    New-DFSNFolderTarget -Path "$NewDFSFolderPath" -TargetPath "$SecondaryTarget" -ReferralPriorityClass GlobalLow
+    dfsutil property acl grant $NewDFSFolderPath $ReadGroup`:RX $WriteGroup`:RX Protect Replace
