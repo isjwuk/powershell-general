8c8173d6d4def4202625db66e5893ca84dbc2925
Chris <aldershotchris@googlemail.com>
2019-02-14 14:54:45 +0000

    Additional examples added

--- lapsafe/lapsafe-api-examples.ps1
+++ lapsafe/lapsafe-api-examples.ps1
@@ -21,7 +21,11 @@ $JSON=Invoke-RestMethod -Method Get -Uri $resource -Header @{ "apikey" = $apiKey
 #Example 1: Show a table of the Installations, their capacity, and current number of devices available
 $JSON.data.installs | Select-Object installName, totalBays, totalAvailable
 
-#Example 2: Extend this to show the Total capacity and devices available across all installations
-$Data=$JSON.data.installs | Select-Object installName, totalBays, totalAvailable
-$TotalAvailable=    ($Data.totalAvailable | Measure-Object -Sum).sum
-$TotalBays=         ($Data.totalBays | Measure-Object -Sum).sum
+#Example 2: Show the Total capacity and devices available across all installations
+$JSON.data | Select-Object totalAvailable, totalBays
+
+#Example 3: Show the number of devices currently on charge in "Installation: A"
+($JSON.data.installs | Where-Object {$_.installName -eq "Installation: A"}).totalOnCharge
+
+#Example 4: Show the name and number of devices available of the installation with the most available devices.
+$JSON.data.installs | Select-Object installName, totalAvailable | Sort-Object -Property totalAvailable -Descending |Select-Object -First 1
